b72ac96cecb0bf99082f0cf7dd5e12ce8c639bdb
Juliet Chirimumimba <chirimumimbajuliet@gmail.com>
2024-08-28 07:04:14 +0200

    few updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import { Routes,Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import EditPage from "./pages/EditPage";
-import Map from "./Map";
 
-import {useJsApiLoader} from '@react-google-maps/api'
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const REACT_APP_GOOGLE_MAPS_API_KEY = import.meta.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 const App = () =>{
   return(
@@ -24,7 +21,7 @@ const App = () =>{
 
       <div className="container mx-auto p-2 h-full">
       <Routes>
-        <Route index element={<Map/>}></Route>
+        <Route index element={<HomePage/>}></Route>
         <Route path="/create" element={<CreatePage/>}></Route>
         <Route path="/edit/:id" element={<EditPage/>}></Route>
       </Routes>
